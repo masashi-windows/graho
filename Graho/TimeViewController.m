d93beecf7a96a1d0ffc03a66962e4792ff9e3b76
masashi-sutou <sutou.masasi@gmail.com>
2015-01-19 23:40:50 +0900

    勤務時間画面の出勤/退勤時間もピッカーで操作可能に変更。 ボタンのみだと間違えて出勤退勤したときに修正ができないため。

--- Graho/TimeViewController.m
+++ Graho/TimeViewController.m
@@ -10,26 +10,41 @@
 
 @interface TimeViewController ()<UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate>
 
+#pragma mark - 現在時刻ラベル宣言
 // 現在時刻
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 
+#pragma mark - 時間のテキストフィールド・ラベル宣言
 // 出勤・休憩・退勤・勤務時間
-@property (weak, nonatomic) IBOutlet UILabel *startTimeLabel;
-@property (retain, nonatomic) IBOutlet UITextField *resetTextField;
-@property (weak, nonatomic) IBOutlet UILabel *finishTimeLabel;
+@property (retain, nonatomic) IBOutlet UITextField *startTimeTextField;
+@property (retain, nonatomic) IBOutlet UITextField *resetTimeTextField;
+@property (retain, nonatomic) IBOutlet UITextField *finishTimeTextField;
 @property (weak, nonatomic) IBOutlet UILabel *workTimeLabel;
 
+
+#pragma mark - ピッカー宣言
+// 出勤時間ピッカー
+@property (nonatomic, retain)   UIPickerView *startPickerView;
+@property (nonatomic, retain)   UIView *startBackView;
+@property (nonatomic, strong)   NSArray *startItems;
+
+// 休憩時間ピッカー
+@property (nonatomic, retain)   UIPickerView *resetPickerView;
+@property (nonatomic, retain)   UIView *resetBackView;
+@property (nonatomic, strong)   NSArray *resetItems;
+
+// 休憩時間の分表示（５分区切り）
+@property (nonatomic, strong)   NSMutableArray *resetMinuteArray;
+
+// 退勤時間ピッカー
+@property (nonatomic, retain)   UIPickerView *finishPickerView;
+@property (nonatomic, retain)   UIView *finishBackView;
+@property (nonatomic, strong)   NSArray *finishItems;
+
+#pragma mark - 出勤・退勤ボタン宣言
 // ボタン
 @property (weak, nonatomic) IBOutlet UIButton *wStartButton;
 @property (weak, nonatomic) IBOutlet UIButton *wFinishButton;
-
-// 休憩時間ピッカー
-@property (nonatomic, retain)   UIPickerView *pickerView;
-@property (nonatomic, retain)   UIView *backView;
-@property (nonatomic, strong)   NSArray *items;
-
-// 休憩時間ピッカーの分表示
-@property (strong, nonatomic) NSMutableArray *minutesArray;
 
 @end
 
@@ -49,8 +64,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    
-    
     // 現在時刻を１秒ごとに表示
     [NSTimer scheduledTimerWithTimeInterval:1.0f
                                      target:self
@@ -60,69 +73,151 @@
     // 現在時刻の更新へ
     [self workTimer];
     
-    
-    
-    // ボタンのデザイン
+
+    // 出勤ボタンのデザイン
     // self.wStartButton.backgroundColor = [UIColor lightGrayColor];
     self.wStartButton.layer.borderColor = [UIColor grayColor].CGColor;
     self.wStartButton.layer.borderWidth = 1.0f;
     self.wStartButton.layer.cornerRadius = 7.5f;
 
+    // 退勤ボタンのデザイン
     self.wFinishButton.layer.borderColor = [UIColor grayColor].CGColor;
     self.wFinishButton.layer.borderWidth = 1.0f;
     self.wFinishButton.layer.cornerRadius = 7.5f;
+
+    
+    // 出勤時間テキストフィールドのデザイン
+    self.startTimeTextField.borderStyle = UITextBorderStyleRoundedRect;
+    self.startTimeTextField.textColor = [UIColor blackColor];
+    self.startTimeTextField.placeholder = @"00:00";
+    self.startTimeTextField.delegate = self;
+    self.startTimeTextField.tag = 1;
     
     // 休憩時間テキストフィールドのデザイン
-    self.resetTextField.borderStyle = UITextBorderStyleRoundedRect;
-    self.resetTextField.textColor = [UIColor blueColor];
-    self.resetTextField.placeholder = @"未選択";
-    self.resetTextField.delegate = self;
+    self.resetTimeTextField.borderStyle = UITextBorderStyleRoundedRect;
+    self.resetTimeTextField.textColor = [UIColor blueColor];
+    self.resetTimeTextField.placeholder = @"未設定";
+    self.resetTimeTextField.delegate = self;
+    self.resetTimeTextField.tag = 2;
+    
+    // 退勤時間テキストフィールドのデザイン
+    self.finishTimeTextField.borderStyle = UITextBorderStyleRoundedRect;
+    self.finishTimeTextField.textColor = [UIColor blackColor];
+    self.finishTimeTextField.placeholder = @"00:00";
+    self.finishTimeTextField.delegate = self;
+    self.finishTimeTextField.tag = 3;
     
     
+    // 出勤時間ピッカーの生成
+    _startPickerView = [[UIPickerView alloc] init];
+    self.startPickerView.delegate = self;
+    self.startPickerView.dataSource = self;
+    self.startPickerView.showsSelectionIndicator = YES;
+    self.startPickerView.tag = 1;
     
     // 休憩時間ピッカーの生成
-    _pickerView = [[UIPickerView alloc] init];
-    self.pickerView.delegate = self;
-    self.pickerView.dataSource = self;
-    self.pickerView.showsSelectionIndicator = YES;
+    _resetPickerView = [[UIPickerView alloc] init];
+    self.resetPickerView.delegate = self;
+    self.resetPickerView.dataSource = self;
+    self.resetPickerView.showsSelectionIndicator = YES;
+    self.resetPickerView.tag = 2;
+    
+    // 退勤時間ピッカーの生成
+    _finishPickerView = [[UIPickerView alloc] init];
+    self.finishPickerView.delegate = self;
+    self.finishPickerView.dataSource = self;
+    self.finishPickerView.showsSelectionIndicator = YES;
+    self.finishPickerView.tag = 3;
     
     // テキストフィールドの選択時にピッカー表示
-    self.resetTextField.inputView = self.pickerView;
+    self.startTimeTextField.inputView = self.startPickerView;
+    self.resetTimeTextField.inputView = self.resetPickerView;
+    self.finishTimeTextField.inputView = self.finishPickerView;
     
-    // 休憩時間ピッカーのツールバー（閉じるボタン）生成
-    UIToolbar *toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
-    toolBar.barStyle = UIBarStyleBlackOpaque;
-    [toolBar sizeToFit];
+    // ピッカーのツールバー（閉じるボタン）生成
+    UIToolbar *startToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    startToolBar.barStyle = UIBarStyleBlackOpaque;
+    [startToolBar sizeToFit];
     
-    UIBarButtonItem *spacer = [[UIBarButtonItem alloc]
-                               initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
-                                                    target:nil
-                                                    action:nil];
+    UIToolbar *resetToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    resetToolBar.barStyle = UIBarStyleBlackOpaque;
+    [resetToolBar sizeToFit];
     
-    UIBarButtonItem *done = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
-                                                                          target:self
-                                                                          action:@selector(donePushed)];
+    UIToolbar *finishToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    finishToolBar.barStyle = UIBarStyleBlackOpaque;
+    [finishToolBar sizeToFit];
     
-    NSArray *items = [NSArray arrayWithObjects:spacer, done, nil];
-    [toolBar setItems:items animated:YES];
+    
+    UIBarButtonItem *startSpacer = [[UIBarButtonItem alloc]
+                                    initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                         target:nil
+                                                         action:nil];
+    
+    UIBarButtonItem *resetSpacer = [[UIBarButtonItem alloc]
+                                    initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                         target:nil
+                                                         action:nil];
+    
+    UIBarButtonItem *finishSpacer = [[UIBarButtonItem alloc]
+                                    initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                         target:nil
+                                                         action:nil];
+
+    UIBarButtonItem *startDone = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                               target:self
+                                                                               action:@selector(startDonePushed)];
+    
+    UIBarButtonItem *resetDone = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                               target:self
+                                                                               action:@selector(resetDonePushed)];
+    
+    UIBarButtonItem *finishDone = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                               target:self
+                                                                               action:@selector(finishDonePushed)];
+
+    
+    NSArray *startItems = [NSArray arrayWithObjects:startSpacer, startDone, nil];
+    [startToolBar setItems:startItems animated:YES];
+    
+    NSArray *resetItems = [NSArray arrayWithObjects:resetSpacer, resetDone, nil];
+    [resetToolBar setItems:resetItems animated:YES];
+    
+    NSArray *finishItems = [NSArray arrayWithObjects:finishSpacer, finishDone, nil];
+    [finishToolBar setItems:finishItems animated:YES];
     
     // テキストフィールドの選択時にツールバー表示
-    self.resetTextField.inputAccessoryView = toolBar;
-    
-    
+    self.startTimeTextField.inputAccessoryView = startToolBar;
+    self.resetTimeTextField.inputAccessoryView = resetToolBar;
+    self.finishTimeTextField.inputAccessoryView = finishToolBar;
+   
+
     // 一枚の黒いUIViewを加えて、ピッカーをモーダル表示のようにする
-    _backView = [[UIView alloc] initWithFrame:self.view.frame];
-    self.backView.backgroundColor = [UIColor blackColor];
-    self.backView.alpha = 0.2;
-    self.backView.hidden = YES;
-    self.backView.userInteractionEnabled = NO;
-    [self.view addSubview:self.backView];
+    _startBackView = [[UIView alloc] initWithFrame:self.view.frame];
+    self.startBackView.backgroundColor = [UIColor blackColor];
+    self.startBackView.alpha = 0.2;
+    self.startBackView.hidden = YES;
+    self.startBackView.userInteractionEnabled = NO;
+    [self.view addSubview:self.startBackView];
+    
+    _resetBackView = [[UIView alloc] initWithFrame:self.view.frame];
+    self.resetBackView.backgroundColor = [UIColor blackColor];
+    self.resetBackView.alpha = 0.2;
+    self.resetBackView.hidden = YES;
+    self.resetBackView.userInteractionEnabled = NO;
+    [self.view addSubview:self.resetBackView];
+    
+    _finishBackView = [[UIView alloc] initWithFrame:self.view.frame];
+    self.finishBackView.backgroundColor = [UIColor blackColor];
+    self.finishBackView.alpha = 0.2;
+    self.finishBackView.hidden = YES;
+    self.finishBackView.userInteractionEnabled = NO;
+    [self.view addSubview:self.finishBackView];
     
     // 休憩時間ピッカーの２列目（NSNumberに変換して配列に格納）
-    self.minutesArray = [NSMutableArray array];
-    NSInteger i;
+    self.resetMinuteArray = [NSMutableArray array];
+    int i;
     for (i=0; i<60; i +=5) {
-        [self.minutesArray addObject:[NSNumber numberWithInteger:i]];
+        [self.resetMinuteArray addObject:[NSNumber numberWithInteger:i]];
     }
 }
 
@@ -185,38 +280,69 @@
 }
 
 
-#pragma mark - 休憩時間ピッカーの処理
+#pragma mark - ピッカーの処理
 
 /**
- *  ピッカーを閉じる
+ *  出勤時間ピッカーを閉じる
  */
-- (void)donePushed
+- (void)startDonePushed
 {
-    self.backView.hidden = YES;
-    [self.resetTextField resignFirstResponder];
+    NSLog(@"%@", @"出勤Done押した。");
+    self.startBackView.hidden = YES;
+    [self.startTimeTextField resignFirstResponder];
+}
+
+/**
+ *  休憩時間ピッカーを閉じる
+ */
+- (void)resetDonePushed
+{
+    NSLog(@"%@", @"休憩Done押した。");
+    self.resetBackView.hidden = YES;
+    [self.resetTimeTextField resignFirstResponder];
+}
+
+/**
+ *  退勤時間ピッカーを閉じる
+ */
+- (void)finishDonePushed
+{
+    NSLog(@"%@", @"退勤Done押した。");
+    self.finishBackView.hidden = YES;
+    [self.finishTimeTextField resignFirstResponder];
 }
 
 /**
  *  テキストフィールドの編集表示
  *
- *  @param textField 休憩時間テキストフィールド
+ *  @param textField 出勤時間テキストフィールド
  *
  *  @return キーボードの表示（YES）
  */
-- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+- (BOOL)TextFieldShouldBeginEditing:(UITextField *)textField
 {
-    self.backView.hidden = NO;
-    
+    switch (textField.tag) {
+        case 1:
+            self.startBackView.hidden = NO;
+            break;
+        case 2:
+            self.resetBackView.hidden = NO;
+            break;
+        case 3:
+            self.finishBackView.hidden = NO;
+            break;
+        default:
+            break;
+    }
     return YES;
 }
-
 
 #pragma mark - UIPikerViewDelegate
 
 /**
  *  ピッカーに表示する列数を返す。(必須)
  *
- *  @param pickerView 休憩時間ピッカー
+ *  @param pickerView ピッカー
  *
  *  @return 表示する列数
  */
@@ -229,28 +355,38 @@
 /**
  *  ピッカーに表示する行数を返す。(必須)
  *
- *  @param pickerView 休憩時間ピッカー
+ *  @param pickerView ピッカー
  *  @param component  列数
  *
  *  @return 表示する行数
  */
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    switch (component) {
-        case 0: return 5;
-        case 1: return [self.minutesArray count];
+    // 休憩時間ピッカーは0~4時間で5分刻み
+    if (pickerView.tag == 2) {
+        switch (component) {
+            case 0: return 5;
+            case 1: return [self.resetMinuteArray count];
+        }
+        return 0;
     }
-    return 0;
+    
+    switch (component) {
+        case 0: return 24;
+        case 1: return 60;
+        default: return 0;
+        break;
+    }
 }
 
 
 /**
  *  行のサイズを指定する。
  *
- *  @param pickerView 休憩時間ピッカー
+ *  @param pickerView ピッカー
  *  @param component  列数
  *
- *  @return <#return value description#>
+ *  @return 行のサイズ
  */
 - (CGFloat)pickerView:(UIPickerView *)pickerView
     widthForComponent:(NSInteger)component
@@ -272,7 +408,7 @@
 /**
  *  ピッカーに表示する値を返す。
  *
- *  @param pickerView 休憩時間ピッカー
+ *  @param pickerView ピッカー
  *  @param row        行数
  *  @param component  列数
  *
@@ -280,25 +416,40 @@
  */
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
+    // 休憩時間ピッカーの表示形式
+    if (pickerView.tag == 2) {
+        switch (component) {
+            case 0: // 1列目
+                return [NSString stringWithFormat:@"%ld",(long)row];
+                break;
+            case 1: // 2列目
+                return [NSString stringWithFormat:@"%d", [[self.resetMinuteArray objectAtIndex:row] intValue]];
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
     
     switch (component) {
         case 0: // 1列目
-            return [NSString stringWithFormat:@"%ld時間",(long)row];
+            return [NSString stringWithFormat:@"%ld",(long)row];
             break;
         case 1: // 2列目
-            return [NSString stringWithFormat:@"%d分", [[self.minutesArray objectAtIndex:row] intValue]];
+            return [NSString stringWithFormat:@"%ld",(long)row];
             break;
         default:
             return 0;
             break;
     }
+    
 }
 
 
 /**
  *  ピッカーで選択時した値を表示する処理
  *
- *  @param pickerView 休憩時間ピッカー
+ *  @param pickerView ピッカー
  *  @param row        行数
  *  @param component  列数
  */
@@ -306,8 +457,24 @@
 {
     // 選択されている時間の行を取得
     int rowOfHour  = (int)[pickerView selectedRowInComponent:0];
-    int rowOfMinutes = (int)[pickerView selectedRowInComponent:1];
+    int rowOfMinute = (int)[pickerView selectedRowInComponent:1];
     
-    self.resetTextField.text = [NSString stringWithFormat:@"%d時間%d分",rowOfHour, [[self.minutesArray objectAtIndex:rowOfMinutes] intValue]];
+    switch (pickerView.tag) {
+        case 1:
+            NSLog(@"%@", @"出勤text変更。");
+            self.startTimeTextField.text = [NSString stringWithFormat:@"%d時%02d分",rowOfHour, rowOfMinute];
+            break;
+        case 2:
+            NSLog(@"%@", @"休憩text変更。");
+            self.resetTimeTextField.text = [NSString stringWithFormat:@"%d時間%d分",rowOfHour, [[self.resetMinuteArray objectAtIndex:rowOfMinute] intValue]];
+            break;
+        case 3:
+            NSLog(@"%@", @"退勤text変更。");
+            self.finishTimeTextField.text = [NSString stringWithFormat:@"%d時%02d分",rowOfHour, rowOfMinute];
+            break;
+        default:
+            break;
+    }
+    
 }
 @end
